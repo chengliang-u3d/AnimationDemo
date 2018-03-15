23700e6906e3cd0dc5da66662501f80d72f922a2
ChengLiang <chengliang@unity3d.com>
2018-03-15 15:13:12 +0800

    add reverse anim

---
+++ Assets/AnimControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+    public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            
+            anim["simpleMove"].speed = 1;
+            //anim["simpleMove"].time = anim["simpleMove"].length;
+            anim.Play("simpleMove");
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            
+            anim["simpleMove"].speed = -1;
+            anim["simpleMove"].time = anim["simpleMove"].length;
+            anim.Play("simpleMove");
+        }
+	}
+}
